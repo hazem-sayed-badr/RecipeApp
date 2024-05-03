7172c74c622f530f0c8ebcb5c93cb62aa20e915d
hazem-sayed-badr <hazem.badr167@gmail.com>
2024-05-03 03:31:47 +0300

    dropdowncommit

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,9 +5,6 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@an
 })
 export class Dropdown{
   @HostBinding('class.open') isOpen=false;
-    constructor(private element:ElementRef, private renderer:Renderer2){
-
-    }
      @HostListener('click')toggleOpen(){
       this.isOpen=!this.isOpen;
      }
